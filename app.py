4e0079981e7e7a44f1adf56fcd2b6b1d076c5011
zjc2106 <92057346+zjc2106@users.noreply.github.com>
2023-09-10 18:28:45 -0400

    cleaned up app.py and updated readme

--- app.py
+++ app.py
@@ -5,6 +5,75 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/", methods = ['POST', 'GET'])
+@app.route("/index.html", methods = ['POST', 'GET'])
+def home():
+    # check if post or get
+    if request.method == "GET":
+        # Configure API request
+        url = "https://developer.nps.gov/api/v1/parks?&api_key={}".format(os.environ.get("NPS_KEY"))
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
+        parks = []
+        # make park data readable to template
+        for park in dict["data"]:
+            if park and len(park["images"])>0:
+                park = {
+                    "name": park["fullName"],
+                    "image": park["images"][0]['url'],
+                    "code": park["parkCode"]
+                }
+                parks.append(park)
+        return render_template ("index.html", parks=parks)
+    else:
+        # post- read form data
+        state = request.form['state']
+        query = request.form['query']
+        query = query.replace(' ', '%20')
+        # configure request
+        if query != "":
+            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&q={}&api_key={}".format(state,query, os.environ.get("NPS_KEY"))
+        else:
+            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}".format(state, os.environ.get("NPS_KEY"))
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
+        parks = []
+        for park in dict["data"]:
+            if park and len(park["images"])>0:
+                park = {
+                    "name": park["fullName"],
+                    "image": park["images"][0]['url'],
+                    "code": park["parkCode"]
+                }
+                parks.append(park)
+        # list to keep track of past form values
+        formInfo = [state, abbrev_to_us_state[state], query.replace('%20',' ')]
+        return render_template ("index.html", parks=parks, formInfo = formInfo)
+
+#path for each specific park
+@app.route('/generic.html/<path:code>')
+def get_specific(code):
+    # Configure API request
+    url = "https://developer.nps.gov/api/v1/parks?parkCode={}&api_key={}".format(code, os.environ.get("NPS_KEY"))
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    # get necessary park data
+    for park in dict["data"]:
+        park = {
+            "name": park["fullName"],
+            "description": park["description"],
+            "states": park["states"],
+            "images": park["images"],
+            "weather": park["weatherInfo"],
+            "topics": park["topics"],
+            "url": park["url"],
+            "address": park["addresses"][0],
+            "activities": park["activities"]
+        }
+    return render_template ("generic.html", park = park)
 
 us_state_to_abbrev = {
     "Alabama": "AL",
@@ -69,163 +138,3 @@ us_state_to_abbrev = {
     
 # invert the dictionary
 abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
-global limit
-limit = 50
-
-@app.route("/", methods = ['POST', 'GET'])
-@app.route("/index.html", methods = ['POST', 'GET'])
-def home():
-    # Configure API request
-    if request.method == "GET":
-        url = "https://developer.nps.gov/api/v1/parks?&api_key={}".format(os.environ.get("NPS_KEY"))
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        dict = json.loads(data)
-        # print(dict)
-        parks = []
-        for park in dict["data"]:
-            if park and len(park["images"])>0:
-                park = {
-                    "name": park["fullName"],
-                    "image": park["images"][0]['url'],
-                    "code": park["parkCode"]
-                }
-                parks.append(park)
-        return render_template ("index.html", parks=parks)
-    else:
-        print(request.form)
-        # if "load" in request.form:
-        #     print(request.form)
-        #     start = start + request.form["load"]
-        # else:
-        #     start = 0
-        state = request.form['state']
-        print(state)
-        query = request.form['query']
-        query = query.replace(' ', '%20')
-        if query != "":
-            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&q={}&api_key={}".format(state,query, os.environ.get("NPS_KEY"))
-        else:
-            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}".format(state, os.environ.get("NPS_KEY"))
-        print(os.environ.get("NPS_KEY"))
-        response = urllib.request.urlopen(url)
-        data = response.read()
-        dict = json.loads(data)
-        # print(dict)
-        parks = []
-        for park in dict["data"]:
-            if park and len(park["images"])>0:
-                park = {
-                    "name": park["fullName"],
-                    "image": park["images"][0]['url'],
-                    "code": park["parkCode"]
-                }
-                parks.append(park)
-        formInfo = [state, abbrev_to_us_state[state], query.replace('%20',' ')]
-        return render_template ("index.html", parks=parks, formInfo = formInfo)
-
-
-# @app.route("/index.html/?<path:code>")
-# def filters(code):
-#     # Configure API request
-#     url = "https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}".format(code, os.environ.get("NPS_KEY"))
-#     print(os.environ.get("NPS_KEY"))
-#     response = urllib.request.urlopen(url)
-#     data = response.read()
-#     dict = json.loads(data)
-#     # print(dict)
-#     parks = []
-#     for park in dict["data"]:
-#         if park and len(park["images"])>0:
-#             park = {
-#                 "name": park["fullName"],
-#                 "image": park["images"][0]['url'],
-#                 "code": park["parkCode"]
-#             }
-#             parks.append(park)
-#     return render_template ("index.html", parks=parks)
-
-@app.route("/generic.html")
-def get_grte():
-    # Configure API request
-    # url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
-    # response = urllib.request.urlopen(url)
-    # data = response.read()
-    # dict = json.loads(data)
-    # parks = []
-    # print(dict)
-    # for park in dict["data"]:
-    #     park = {
-    #         "Park Name": park["fullName"],
-    #         "Description": park["description"],
-    #         "State": park["states"],
-    #         "Image": park["images"][0]['url']
-    #     }
-    #     parks.append(park)
-
-    return render_template ("generic.html")
-
-
-
-@app.route("/elements.html")
-def get_elements():
-    # Configure API request
-    # url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
-    # response = urllib.request.urlopen(url)
-    # data = response.read()
-    # dict = json.loads(data)
-    # parks = []
-    # # print(dict)
-    # for park in dict["data"]:
-    #     park = {
-    #         "Park Name": park["fullName"],
-    #         "Description": park["description"],
-    #         "State": park["states"],
-    #         "Image": park["images"][0]['url']
-    #     }
-    #     parks.append(park)
-
-    return render_template ("elements.html")
-
-
-@app.route("/api")
-def get_seeInfo():
-    # Configure API request
-    url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
-    parks = []
-    # print(dict)
-    for park in dict["data"]:
-        # park = {
-        #     "Park Name": park["fullName"],
-        #     "Description": park["description"],
-        #     "State": park["states"],
-        #     "Image": park["images"][0]['url']
-        # }
-        parks.append(park)
-
-    return {"results": parks}
-
-# @app.route("/generic.html/", defaults={"code":""})
-@app.route('/generic.html/<path:code>')
-def get_specific(code):
-    # Configure API request
-    url = "https://developer.nps.gov/api/v1/parks?parkCode={}&api_key={}".format(code, os.environ.get("NPS_KEY"))
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
-    for park in dict["data"]:
-        park = {
-            "name": park["fullName"],
-            "description": park["description"],
-            "states": park["states"],
-            "images": park["images"],
-            "weather": park["weatherInfo"],
-            "topics": park["topics"],
-            "url": park["url"],
-            "address": park["addresses"][0],
-            "activities": park["activities"]
-        }
-    return render_template ("generic.html", park = park)
--- readme.md
+++ readme.md
@@ -1,7 +1,24 @@
-Steps to run:
+# National Park Explorer
 
-1. open directory
-2. open terminal
-3. use command *source .venv/bin/activate* to get into virtual env
-4. add API key using *export NPS_KEY="API_KEY_HERE*
-5. Run Flask app using *python -m flask run*, if in development mode use *export FLASK_ENV=development*
+Welcome to the National Park Explorer project. This web application is designed to help users discover and explore information about the 423 National Park sites across the United States. Whether you're planning your next outdoor adventure or simply curious about these natural treasures, National Park Explorer makes it easy.
+
+## Features
+
+- **Efficient Filtering**: Filter parks by location, activities, topics, and keywords to find the perfect match for your interests.
+
+- **Detailed Park Information**: Get comprehensive details about each National Park site, from weather, activities, and interests.
+
+- **User-Friendly Interface**: A user-friendly interface makes navigating and finding information a breeze.
+
+## Technologies Used
+
+- **Frontend**: HTML, CSS, JavaScript
+- **Backend**: Python (Flask)
+- **Data Source**: National Park Service REST API
+- **Deployment**: pythonanywhere.com
+
+## Usage
+
+1. Visit (zjc2106.pythonanywhere.com) to access the web app.
+2. Use the intuitive filters to refine your park search.
+3. Click on a park to view in-depth information.
