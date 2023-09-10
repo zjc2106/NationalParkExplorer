615ecd96fe6fecea268a5c9af47f55d122cb57c0
zjc2106 <92057346+zjc2106@users.noreply.github.com>
2023-09-10 14:20:09 -0400

    Updated app.py. Added CSS, JS.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import urllib.request, json
 
 import os
@@ -6,27 +6,190 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/")
-def test():
-    # Configure API request
-    url = "https://developer.nps.gov/api/v1/parks?stateCode=CA&api_key={}".format(os.environ.get("NPS_KEY"))
-    print(os.environ.get("NPS_KEY"))
-    response = urllib.request.urlopen(url)
-    data = response.read()
-    dict = json.loads(data)
-    # print(dict)
-    parks = []
-    for park in dict["data"]:
-        if park and len(park["images"])>0:
-            park = {
-                "name": park["fullName"],
-                "image": park["images"][0]['url']
-            }
-            parks.append(park)
-    return render_template ("index.html", parks=parks)
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+    "All 50": ""
+}
+    
+# invert the dictionary
+abbrev_to_us_state = dict(map(reversed, us_state_to_abbrev.items()))
+global limit
+limit = 50
 
-@app.route("/grte")
+@app.route("/", methods = ['POST', 'GET'])
+@app.route("/index.html", methods = ['POST', 'GET'])
+def home():
+    # Configure API request
+    if request.method == "GET":
+        url = "https://developer.nps.gov/api/v1/parks?&api_key={}".format(os.environ.get("NPS_KEY"))
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
+        # print(dict)
+        parks = []
+        for park in dict["data"]:
+            if park and len(park["images"])>0:
+                park = {
+                    "name": park["fullName"],
+                    "image": park["images"][0]['url'],
+                    "code": park["parkCode"]
+                }
+                parks.append(park)
+        return render_template ("index.html", parks=parks)
+    else:
+        print(request.form)
+        # if "load" in request.form:
+        #     print(request.form)
+        #     start = start + request.form["load"]
+        # else:
+        #     start = 0
+        state = request.form['state']
+        print(state)
+        query = request.form['query']
+        query = query.replace(' ', '%20')
+        if query != "":
+            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&q={}&api_key={}".format(state,query, os.environ.get("NPS_KEY"))
+        else:
+            url =  "https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}".format(state, os.environ.get("NPS_KEY"))
+        print(os.environ.get("NPS_KEY"))
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
+        # print(dict)
+        parks = []
+        for park in dict["data"]:
+            if park and len(park["images"])>0:
+                park = {
+                    "name": park["fullName"],
+                    "image": park["images"][0]['url'],
+                    "code": park["parkCode"]
+                }
+                parks.append(park)
+        formInfo = [state, abbrev_to_us_state[state], query.replace('%20',' ')]
+        return render_template ("index.html", parks=parks, formInfo = formInfo)
+
+
+# @app.route("/index.html/?<path:code>")
+# def filters(code):
+#     # Configure API request
+#     url = "https://developer.nps.gov/api/v1/parks?stateCode={}&api_key={}".format(code, os.environ.get("NPS_KEY"))
+#     print(os.environ.get("NPS_KEY"))
+#     response = urllib.request.urlopen(url)
+#     data = response.read()
+#     dict = json.loads(data)
+#     # print(dict)
+#     parks = []
+#     for park in dict["data"]:
+#         if park and len(park["images"])>0:
+#             park = {
+#                 "name": park["fullName"],
+#                 "image": park["images"][0]['url'],
+#                 "code": park["parkCode"]
+#             }
+#             parks.append(park)
+#     return render_template ("index.html", parks=parks)
+
+@app.route("/generic.html")
 def get_grte():
+    # Configure API request
+    # url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
+    # response = urllib.request.urlopen(url)
+    # data = response.read()
+    # dict = json.loads(data)
+    # parks = []
+    # print(dict)
+    # for park in dict["data"]:
+    #     park = {
+    #         "Park Name": park["fullName"],
+    #         "Description": park["description"],
+    #         "State": park["states"],
+    #         "Image": park["images"][0]['url']
+    #     }
+    #     parks.append(park)
+
+    return render_template ("generic.html")
+
+
+
+@app.route("/elements.html")
+def get_elements():
+    # Configure API request
+    # url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
+    # response = urllib.request.urlopen(url)
+    # data = response.read()
+    # dict = json.loads(data)
+    # parks = []
+    # # print(dict)
+    # for park in dict["data"]:
+    #     park = {
+    #         "Park Name": park["fullName"],
+    #         "Description": park["description"],
+    #         "State": park["states"],
+    #         "Image": park["images"][0]['url']
+    #     }
+    #     parks.append(park)
+
+    return render_template ("elements.html")
+
+
+@app.route("/api")
+def get_seeInfo():
     # Configure API request
     url = "https://developer.nps.gov/api/v1/parks?parkCode=grte&api_key={}".format(os.environ.get("NPS_KEY"))
     response = urllib.request.urlopen(url)
@@ -35,12 +198,34 @@ def get_grte():
     parks = []
     # print(dict)
     for park in dict["data"]:
-        park = {
-            "Park Name": park["fullName"],
-            "Description": park["description"],
-            "State": park["states"],
-            "Image": park["images"][0]['url']
-        }
+        # park = {
+        #     "Park Name": park["fullName"],
+        #     "Description": park["description"],
+        #     "State": park["states"],
+        #     "Image": park["images"][0]['url']
+        # }
         parks.append(park)
 
     return {"results": parks}
+
+# @app.route("/generic.html/", defaults={"code":""})
+@app.route('/generic.html/<path:code>')
+def get_specific(code):
+    # Configure API request
+    url = "https://developer.nps.gov/api/v1/parks?parkCode={}&api_key={}".format(code, os.environ.get("NPS_KEY"))
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    for park in dict["data"]:
+        park = {
+            "name": park["fullName"],
+            "description": park["description"],
+            "states": park["states"],
+            "images": park["images"],
+            "weather": park["weatherInfo"],
+            "topics": park["topics"],
+            "url": park["url"],
+            "address": park["addresses"][0],
+            "activities": park["activities"]
+        }
+    return render_template ("generic.html", park = park)
